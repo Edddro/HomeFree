81510e7fe0dca7aa8e40164ff7e2c1833b7a346c
Sean Song <seansongss@gmail.com>
2025-02-16 04:18:47 -0500

    setting wip

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
+const profiles = [
+    { name: "Edward D.", image: "/profile1.jpg" },
+    { name: "Krishna C.", image: "/profile2.jpg" },
+    { name: "Elizabeth W.", image: "/profile3.jpg" },
+];
+
 const page = () => {
-  return (
-    <div>page</div>
-  )
+    return (
+        <div>page</div>
+    )
 }
 
 export default page
